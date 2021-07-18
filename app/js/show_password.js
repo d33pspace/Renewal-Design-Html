371abd6433b8abd09adeeaed757ffea4be7612d3
“Anton <ankarfromhell@gmail.com>
2021-07-19 01:54:51 +0300

    done with getting started and did almost all of main page

---
+++ app/js/show_password.js
@@ -0,0 +1,13 @@
+let showPass = document.querySelectorAll('#showPassword')
+let pass = document.querySelectorAll('.password')
+
+showPass.forEach((item) => {
+    item.onclick = (e) => {
+        if (item.previousElementSibling.type == 'password') {
+            item.previousElementSibling.type = 'text'
+        } else if (item.previousElementSibling.type == 'text') {
+            item.previousElementSibling.type = 'password'
+        }
+    }
+})
+
