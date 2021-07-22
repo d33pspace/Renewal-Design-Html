e0c45ac4f8c195a9e2dfbc8e3f7fdc2a9d027625
“Anton <ankarfromhell@gmail.com>
2021-07-22 10:28:54 +0300

    new guest

---
+++ app/js/guest_dropdown.js
@@ -0,0 +1,15 @@
+let dropdown = document.querySelectorAll('.dropdown')
+
+dropdown.forEach((item) => {
+    item.firstElementChild.onclick = () => {
+        item.classList.toggle('active')
+        item.lastElementChild.classList.toggle('active')
+    }
+    item.lastElementChild.childNodes.forEach((e) => {
+        e.onclick = () => {
+            item.firstElementChild.classList.add('filled')
+            item.lastElementChild.classList.remove('active')
+            item.firstElementChild.firstElementChild.innerHTML = e.innerHTML
+        }
+    })
+})
