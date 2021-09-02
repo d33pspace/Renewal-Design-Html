bc7850d95d5a160ce1879a053cae9b30de3cdaab
“Anton <ankarfromhell@gmail.com>
2021-09-03 01:01:21 +0300

    fixes

---
+++ app/js/clear_guest_list.js
@@ -0,0 +1,23 @@
+let clear_button = document.querySelector('#clear_guest_list')
+let overlay = document.querySelector('.overlay')
+let close_overlay = document.querySelector('#close_overlay')
+let clear_proceed = document.querySelector('#clear_proceed')
+let message = document.querySelector('.message')
+
+clear_button.onclick = () => {
+    toggleOverlay()
+}
+
+close_overlay.onclick = () => {
+    toggleOverlay()
+}
+
+clear_proceed.onclick = () => {
+    toggleOverlay()
+    message.classList.add('active')
+    setInterval(() =>  message.classList.remove('active') , 5000)
+}
+
+function toggleOverlay () {
+    overlay.classList.toggle('active')
+}
