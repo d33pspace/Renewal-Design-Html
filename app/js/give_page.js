cf970fe6ec4c2de7094d00d0c0e5a34514c0571d
Anton Karpov <ankarfromhell@gmail.com>
2021-09-18 01:19:55 +0300

    fixes

--- app/js/give_page.js
+++ app/js/give_page.js
@@ -2,12 +2,15 @@ let item = document.querySelectorAll('.card-content .item')
 let disOverlay = document.querySelector('.overlay.distribution')
 
 item.forEach(e => {
-    e.onclick = () => {
-        disOverlay.classList.add('active')
-        disOverlay.firstElementChild.classList.add('active')
-        if (document.body.scrollHeight !== document.documentElement.offsetHeight) {
-            disOverlay.classList.add('scroll')
+    e.onclick = (i) => {
+        if(i.target !== e.querySelector('.add')) {
+            disOverlay.classList.add('active')
+            disOverlay.firstElementChild.classList.add('active')
+            if (document.body.scrollHeight !== document.documentElement.offsetHeight) {
+                disOverlay.classList.add('scroll')
+            }
         }
+        
     }
 })
 
@@ -21,5 +24,20 @@ overlay.forEach(e => {
         if (i.target == disOverlay) {
             disOverlay.firstElementChild.classList.remove('active')
         }
+    }
+})
+
+
+// giving list logic
+
+let add = document.querySelectorAll('.add')
+
+add.forEach(item => {
+    item.onclick = () => {
+        document.querySelector('.giving_list').classList.add('active')
+        let cardData = item.parentElement.parentElement.parentElement.getAttribute('card-data')
+        let card = document.querySelector('.giving_list').querySelector('*[card-data="' + cardData + '"]')
+        card.classList.add('active')
+        card.querySelector('.counter').innerHTML++
     }
 })
