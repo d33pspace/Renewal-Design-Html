996181c82d7bcb5fcb3a0aa760a47ecd45da6cd8
Anton Karpov <ankarfromhell@gmail.com>
2021-09-19 15:29:43 +0300

    fixes

--- app/js/open_tab_catalogue.js
+++ app/js/open_tab_catalogue.js
@@ -3,10 +3,16 @@ let card = document.querySelectorAll('.card')
 card.forEach((item => {
     if (!item.querySelector('.card-extend').hasChildNodes()) {
         item.classList.add('no-extend')
-    }
-    item.onclick = () => {
-        if(item.querySelector('.arrow')) {
-            item.classList.toggle('active')
+        item.onclick = () => {
+            location.href='./edit_item.html'
+        }
+    } else {
+        item.onclick = (e) => {
+            if (e.target !== item.querySelector('.card-extend .item') & e.target !== item.querySelector('.card-extend .count') & e.target !== item.querySelector('.card-extend .title')) {
+                item.classList.toggle('active')
+            } else {
+                location.href='./edit_item.html'
+            }
         }
     }
 }))
