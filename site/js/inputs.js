20fbf9791177770e6c1d230878fe8c04dbb5a6ee
“Anton <ankarfromhell@gmail.com>
2022-06-27 12:28:45 +0300

    site changes

--- site/js/inputs.js
+++ site/js/inputs.js
@@ -1,10 +1,10 @@
 let input = document.querySelectorAll('.input-wrap .input')
 
-let email = document.querySelector('#email')
-let emailCheckbox = document.querySelector('#emailcheckbox')
+// let email = document.querySelector('#email')
+// let emailCheckbox = document.querySelector('#emailcheckbox')
 
-let address = document.querySelector('#address')
-let addressCheckbox = document.querySelector('#pmailcheckbox')
+// let address = document.querySelector('#address')
+// let addressCheckbox = document.querySelector('#pmailcheckbox')
 
 let textarea = document.querySelector('.input-wrap textarea')
 
@@ -15,22 +15,30 @@ input.forEach(item => {
             item.classList.add('filled')
         }
     }
+    // item.onblur = () => {
+    //     if (item.value == '' && item == email) {
+    //         item.classList.remove('active')
+    //         emailCheckbox.setAttribute('disabled', 'disabled')
+    //         emailCheckbox.checked = false
+    //     }  else if (item == email) {
+    //         emailCheckbox.removeAttribute('disabled')
+    //     } else if (item.value == '' && item == address) {
+    //         item.classList.remove('filled')
+    //         item.classList.remove('active')
+    //         addressCheckbox.setAttribute('disabled', 'disabled')
+    //         addressCheckbox.checked = false
+    //     } else if (item == address) {
+    //         item.classList.remove('filled')
+    //         addressCheckbox.removeAttribute('disabled')
+    //     } else if (item.value == '') {
+    //         item.classList.remove('active')
+    //     }
+    // }
     item.onblur = () => {
-        if (item.value == '' && item == email) {
-            item.classList.remove('active')
-            emailCheckbox.setAttribute('disabled', 'disabled')
-            emailCheckbox.checked = false
-        }  else if (item == email) {
-            emailCheckbox.removeAttribute('disabled')
-        } else if (item.value == '' && item == address) {
+         if (item == address) {
             item.classList.remove('filled')
-            item.classList.remove('active')
-            addressCheckbox.setAttribute('disabled', 'disabled')
-            addressCheckbox.checked = false
-        } else if (item == address) {
-            item.classList.remove('filled')
-            addressCheckbox.removeAttribute('disabled')
-        } else if (item.value == '') {
+        }
+        if (item.value == '') {
             item.classList.remove('active')
         }
     }
