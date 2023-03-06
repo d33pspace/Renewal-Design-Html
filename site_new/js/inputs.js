19269df88983de5106cb955f1ea01aed08ee4dd0
anton karpov <ankarfromhell@gmail.com>
2023-03-06 17:49:04 +0200

    fixes

--- site_new/js/inputs.js
+++ site_new/js/inputs.js
@@ -1,4 +1,5 @@
 let input = document.querySelectorAll('.input-wrap .input')
+let allFilled = false
 
 input.forEach(item => {
     item.onfocus = () => {
@@ -7,9 +8,18 @@ input.forEach(item => {
     }
 
     item.onblur = () => {
+        item.nodeName == 'TEXTAREA' && allFilled ? document.querySelector('#contact_us_form').submit() : 
         item.nodeName == 'TEXTAREA' ? item.classList.remove('filled') : ''
         if (item.value == '') {
             item.classList.remove('active')
+        }
+        for (var i = 0; i < input.length; i++) {
+            if(input[i].value == '') {
+                allFilled = false
+                break
+            } else {
+                allFilled = true
+            }
         }
     }
 })
