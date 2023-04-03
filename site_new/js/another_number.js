cf07153f39ca8ca1d54e4f0e9532a3d65d178607
anton karpov <ankarfromhell@gmail.com>
2023-04-03 14:41:54 +0300

    fixes

--- site_new/js/another_number.js
+++ site_new/js/another_number.js
@@ -3,11 +3,13 @@ let buttons = document.querySelectorAll('.button.inactive')
 
 options.forEach(item => {
     item.oninput = (e) => {
+        console.log(fieldValid(options))
         if (fieldValid(options)) {
             buttons.forEach(i => i.classList.remove('inactive'))
             document.querySelector('.buttons-message').classList.add('active')
         } else {
             buttons.forEach(i => i.classList.add('inactive'))
+            document.querySelector('.buttons-message').classList.remove('active')
         }
         if (item.nextElementSibling.nextElementSibling) {
             if (e.target.value !== '') {
@@ -22,13 +24,13 @@ options.forEach(item => {
 
 function fieldValid(inputs) {
     let allFilled = false
-    inputs.forEach(i => {
-        if (i.value == '') {
+    for (let i = 0; i < inputs.length; i++) { // выведет 0, затем 1, затем 2
+        if (inputs[i].value == '') {
             allFilled = false
             return allFilled
         } else {
             allFilled = true
         }
-    })
+    }
     return allFilled
 }
